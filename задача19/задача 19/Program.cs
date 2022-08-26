9e377bceb477386d77f52a905ceb940e857ce304
Helen <elenaprofessor@yandex.ru>
2022-08-26 12:33:45 +0300

    как просили решена математически без ввода строчек

---
+++ задача19/задача 19/Program.cs
@@ -0,0 +1,18 @@
+﻿// определить плилиндром.
+Console.WriteLine("Введите пятизначное число ");
+double a = double.Parse(Console.ReadLine());
+double s = a%100;
+double b = (a%1000);
+double c = (a-b)/1000;
+double f = c%10*10;
+double e = c%100;
+double x = e%10;
+double r = (e-x)/10;
+double v = f+r;
+if(v==s){
+    Console.WriteLine(a + "=> да");
+}
+if((v<s)&&(v>s)){
+    Console.WriteLine(a + "=> нет");
+}
+         
